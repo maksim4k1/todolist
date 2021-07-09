19f406b61e3e0248cf3fe92c4781c6d1c657bdc7
Maksim Bazhenov <maksim4k1@gmail.com>
2021-07-09 18:04:34 +0600

    added edit task modal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Add tast modal
 const addNewTaskButton = document.querySelector(".todolist__button");
 const addNewTaskCloseButton = document.querySelector(".add-new-task__close-button");
 const addNewTaskBlock = document.querySelector(".add-new-task");
@@ -12,7 +13,26 @@ addNewTaskBlock.addEventListener("click", function (event) {
     if(event.target === event.currentTarget){
         toggleModal("add-new-task");
     }    
-})
+});
+
+// Edit task modal
+const editTaskButton = document.querySelector(".todolist__task-button_edit-task");
+const editTaskCloseButton = document.querySelector(".edit-task__close-button");
+const editTaskBlock = document.querySelector(".edit-task");
+
+let openingModal;
+editTaskButton.addEventListener("click", function(){
+    toggleModal(`${this.classList[0]}~.edit-task`);
+    openingModal = `${this.classList[0]}~.edit-task`;
+});
+editTaskCloseButton.addEventListener("click", function(){
+    toggleModal(openingModal);
+});
+editTaskBlock.addEventListener("click", function (event) {
+    if(event.target === event.currentTarget){
+        toggleModal(openingModal);
+    }    
+});
 
 function toggleModal(modalClass) {
     let modalBlock = document.querySelector(`.${modalClass}`);
